d2422db6f7ef1588fa660f215bd3d69eb851079c
Scott Neel <scott@neel.net>
2016-08-23 10:07:42 -0700

    * add forecast stub

--- src/public/scripts/forecast.js
+++ src/public/scripts/forecast.js
@@ -7,6 +7,54 @@ forecast.config(function($stateProvider, $urlRouterProvider) {
     .state('main.forecast', {
       url: '/Forecast',
       templateUrl: 'views/main/forecast.html',
+      controller: 'forecastController',
+      resolve: {
+        'myAccounts': function ($q, $stateParams, Accounts) {
+          var account,
+            defer = $q.defer();
+
+          accounts = Accounts.query(function () {
+            defer.resolve(accounts);
+          });
+
+          return defer.promise;
+        },
+        'account': function ($q, $stateParams, Accounts) {
+          return {};
+        },
+      }
+    })
+    .state('main.viewforecast', {
+      url: '/Forecast/:id',
+      templateUrl: 'views/main/forecast.view.html',
+      controller: 'forecastController',
+      resolve: {
+        'myAccounts': function ($q, $stateParams, Accounts) {
+          var account,
+            defer = $q.defer();
+
+          accounts = Accounts.query(function () {
+            defer.resolve(accounts);
+          });
+
+          return defer.promise;
+        },
+        'account': function ($q, $stateParams, Accounts) {
+          var account,
+            defer = $q.defer();
+
+          account = Accounts.get({ id: $stateParams.id}, function () {
+            defer.resolve(account);
+          });
+
+          return defer.promise;
+        },
+      }
     });
 
 });
+
+forecast.controller('forecastController', ['$scope', 'myAccounts', 'account', function ($scope, accounts, account) {
+  $scope.accounts = accounts;
+  $scope.account = account;
+}]);
