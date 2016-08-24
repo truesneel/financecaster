8c975dbadf73f22d4ae687c9e586642cf6b28ab3
Scott Neel <scott@neel.net>
2016-08-23 20:20:25 -0700

    * stub out forecasts

---
+++ src/public/apidoc/api_project_bishop_Aug-23-175023-2016_Conflict.js
@@ -0,0 +1,14 @@
+define({
+  "title": "FinanceCaster API Documentation",
+  "description": "API Documentation",
+  "version": "0.0.1",
+  "name": "FinanceCaster",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-08-23T16:10:36.437Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
